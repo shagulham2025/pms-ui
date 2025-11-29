dea58b96b470b08c582be340c75da1ec7ba8e6b5
shagulham2025 <shagulham2025@gmail.com>
2025-11-29 13:29:32 +0530

    feat: Implement change password and profile components with routing and styling

--- pms-ui/src/app/component/configuration/configuration.routes.ts
+++ pms-ui/src/app/component/configuration/configuration.routes.ts
@@ -2,10 +2,14 @@ import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { UsersComponent } from "./users/users-component";
 import { PropertiesComponent } from "./properties/properties-component";
+import { Profile } from "./profile/profile";
+import { ChangePassword } from "./change-password/change-password";
 
 const routes: Routes = [
-     { path: 'users', component: UsersComponent },
-     { path: 'properties', component: PropertiesComponent } 
+    { path: 'users', component: UsersComponent },
+    { path: 'properties', component: PropertiesComponent } ,
+    { path: 'profile', component: Profile },
+    { path: 'change-password', component: ChangePassword },
 ];
 
 @NgModule({
